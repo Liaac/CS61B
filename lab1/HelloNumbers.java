b10107d378b63155ac115c79f1d857317342f346
Liaac <2033010@tongji.edu.cn>
2021-11-06 12:16:39 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers{
+	public static void main(String[] args){
+		int x = 0;
+		int cnt = 0;
+		while(cnt<10){
+			x += cnt;
+			System.out.print(x+' ');
+			cnt++;
+		}
+	}
+}
